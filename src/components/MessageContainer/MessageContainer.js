758d144717beecc73c4db1ed46c39e065f1e2e9e
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-09 20:28:10 -0500

    Added nav for chat (will add docs later)

--- src/components/MessageContainer/MessageContainer.js
+++ src/components/MessageContainer/MessageContainer.js
@@ -8,6 +8,11 @@ import './MessageContainer.css'
 export default function MessageContainer(props) {
    return (
          <div className="message-container col-xs-12 col-sm-4 col-md-3">
+            <div className="row">
+               <ul className="nav nav-pills">
+                  <li className="active"><a>Chat</a></li>
+               </ul>
+            </div>
             <MessageList messages={props.messages} />
             <MessageInput emitMessage={props.emitMessage} />
          </div>
