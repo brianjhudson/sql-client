1a82210de5a8043f9448abcacd1e54fe041cddd6
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-09 20:31:35 -0500

    Added query fields to provide more details in results

--- src/ducks/sql.js
+++ src/ducks/sql.js
@@ -3,29 +3,35 @@ import axios from 'axios'
 // Initial State
 const initialState = {
    fetchingData: false,
-   fetchError: null,
+   error: null,
    tables: [],
-   results: [],
-   fields: [],
    history: [],
+   command: "",
+   rowCount: null,
+   fields: [],
+   results: [],
+   currentQuery: ""
 }
 
 // Actions
 const FETCHING_DATA = 'sql/FETCHING_DATA'
 const FETCH_ERROR = 'sql/FETCH_ERROR'
 const FETCHED_DATA = 'sql/FETCHED_DATA'
+const FETCHED_QUERY = 'sql/FETCHED_QUERY'
 const ADD_DATA = 'sql/ADD_DATA'
+const UPDATE_QUERY = 'sql/UPDATE_QUERY'
 
 // Async Action Creators
 
 export function getTables() {
    return dispatch => {
       dispatch(fetchingData())
-      return axios.get('/api/sql/table')
+      return axios.get('http://localhost:4000/api/sql/table')
       .then(result => {
          dispatch(fetchedData('tables', result.data));
       })
       .catch(err => {
+         console.log(err)
          dispatch(fetchError(err))
       })
    }
@@ -33,12 +39,19 @@ export function getTables() {
 
 export function postQuery(query) {
    return dispatch => {
-      dispatch(addData('history', query))
       dispatch(fetchingData())
-      return axios.post('/api/sql/query', {query: query})
+      dispatch(updateQuery(query))
+      return axios.post('http://localhost:4000/api/sql/query', {query: query})
       .then(result => {
-         dispatch(fetchedData('fields', result.data.fields))
-         dispatch(fetchedData('results', result.data.rows))
+         if (result.data.error) {
+            dispatch(fetchError(result.data))
+         } else {
+            dispatch(fetchedQuery(query, result.data))
+            dispatch(getTables())
+         }
+      })
+      .catch(err => {
+         dispatch(fetchError(err))
       })
    }
 }
@@ -65,6 +78,14 @@ function fetchedData(key, data) {
    }
 }
 
+function fetchedQuery(query, data) {
+   return {
+      type: FETCHED_QUERY,
+      query,
+      data
+   }
+}
+
 export function addData(key, data) {
    return {
       type: ADD_DATA,
@@ -73,19 +94,50 @@ export function addData(key, data) {
    }
 }
 
+export function updateQuery(query) {
+   return {
+      type: UPDATE_QUERY,
+      query
+   }
+}
+
 export default function sqlReducer(state = initialState, action) {
    console.log(action.type)
    switch(action.type) {
       case FETCHING_DATA: 
          return Object.assign({}, state, {fetchingData: true})
-      case FETCH_ERROR: 
-         return Object.assign({}, state, {fetchingData: false, fetchError: action.error})
+      case FETCH_ERROR:
+         return Object.assign({}, state, {
+            fetchingData: false, 
+            error: action.error,
+            command: null,
+            rowCount: null,
+            results: [],
+            fields: []
+         })
       case FETCHED_DATA: 
          return Object.assign({}, state, {fetchingData: false, [action.key]: action.data})
+      case FETCHED_QUERY:
+         const {command, rowCount, rows, fields} = action.data
+         const query = action.query.toUpperCase()
+         const history = state.history.slice()
+         if (state.history.indexOf(query) === -1) {
+            history.push(query)
+         }
+         return Object.assign({}, state, {
+            history,
+            command,
+            rowCount,
+            results: rows,
+            fields,
+            error: null
+         })
       case ADD_DATA:
          const newArr = state[action.key].slice()
          newArr.push(action.data)
          return Object.assign({}, state, {[action.key]: newArr})
+      case UPDATE_QUERY:
+         return Object.assign({}, state, {currentQuery: action.query.toUpperCase()})
       default:
          return state
    }   
