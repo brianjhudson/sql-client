3055b9e05f774d42c6e781943a9f9cca5e6d8ea7
Brian Hudson <brianjasonhudson@gmail.com>
2017-07-09 20:29:12 -0500

    Added user and signin/out to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {logoutUser} from '../../ducks/user'
 
 import './Nav.css'
 
-export default function() {
+function Nav(props) {
    return (
       <div className="navbar navbar-inverse">
          <div className="container-fluid">
@@ -14,17 +16,23 @@ export default function() {
                   <span className="icon-bar"></span>
                   <span className="icon-bar"></span>
                </button>
-               <Link className="navbar-brand" to="/">
-                  Brian's SQL App
-               </Link>
+               <Link className="navbar-brand" to="/">PG Playground</Link>
             </div>
             <div className="collapse navbar-collapse" id="mobile-nav">
-               <ul className="nav navbar-nav">
-                  <li>
-                     <Link to="/">Login</Link>
-                  </li>
-                  <li>
-                     <Link to="/dashboard">Dashboard</Link>
+               <ul className="nav navbar-nav navbar-right">
+                  <li className="profile-container dropdown">
+                  {
+                     props.user.github_username === 'guestuser'
+                     ?
+                        <a className="auth-link" href="http://localhost:4000/auth/github">
+                           <img src={props.user.github_avatar} alt=""/>
+                        </a>
+                     :
+                        <a className="auth-link" onClick={this.props.logoutUser}>
+                           <img src={props.user.github_avatar} alt=""/>
+                        </a>
+
+                  }
                   </li>
                </ul>
             </div>
@@ -32,3 +40,12 @@ export default function() {
       </div>
    )
 }
+
+function mapStateToProps(state) {
+   return state.user
+}
+
+const mapDispatchToProps = {
+   logoutUser
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Nav)
